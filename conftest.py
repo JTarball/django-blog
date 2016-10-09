bc383088db4c91c6f92266457c2b23dd3e3bc19c
James Tarball <james.tarball@gmail.com>
2016-10-09 15:47:38 +0100

    forgot to change contest.py

--- conftest.py
+++ conftest.py
@@ -6,9 +6,9 @@ import os
 
 from django.conf import settings
 
-from tests import settings as test_settings
+from django_blog.tests import settings as test_settings
 
 
 def pytest_configure():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tests.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_blog.tests.settings')
     settings.configure(default_settings=test_settings)
